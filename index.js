06ffa827251556436839e8564bb05d967e9d454a
James <>
2023-07-10 21:28:18 -0500

    added start scripts and landing page for get /posts route

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/post', (req, res, next) => {
+app.get('/posts', (req, res, next) => {
     console.log('You made a request to the "post" route');
+    res.status(200).send("Welcome to the posts page!");
 });
 
 app.listen(3000, () => {
